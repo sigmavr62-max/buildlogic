20185056c49fce02597e358d947207ced9cf5a66
shareavm <sigmavr62@gmail.com>
2026-03-28 15:03:03 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
+// api/index.js
+let savedText = "Hello World";
 
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    // This is where your Typewriter Menu sends data
+    savedText = req.body.text;
+    return res.status(200).send("Text Updated");
+  } else {
+    // This is where Build Logic "fetches" the data
+    return res.status(200).send(savedText);
+  }
+}
